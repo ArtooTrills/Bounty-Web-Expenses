c3f5c24da683d6d477e43e5e737393bfacdefa6f
archugs <archana.gurusamy@gmail.com>
2014-04-02 23:33:32 +0530

    Added Export to Excel and Print table options

--- js/controllers/summary.js
+++ js/controllers/summary.js
@@ -1,4 +1,25 @@
 App.SummaryController = Ember.ArrayController.extend({
 	content: [],
-	itemController: 'summaryItem'
+	itemController: 'summaryItem',
+	actions: {
+		exportToExcel: function() {
+			var htmltable = document.getElementById('summary');
+			var html = htmltable.outerHTML;
+			window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));	
+		},
+		printTable: function() {
+			var htmltable = document.getElementById('summary');
+			newWin = window.open("");
+			newWin.document.writeln("<html><head>");
+			newWin.document.writeln('<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />');
+			newWin.document.writeln("<link rel='stylesheet' type='text/css' href='css/style.css'>");
+			newWin.document.writeln("</head><body>");
+			newWin.document.writeln(htmltable.outerHTML);
+			newWin.document.writeln("</body></html>");
+			newWin.document.close();
+			newWin.focus();
+			newWin.print();
+			newWin.close();
+		}
+	}
 });
