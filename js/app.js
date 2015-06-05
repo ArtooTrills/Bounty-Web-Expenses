c12e75bb478d738a043ac56a06af7bd5684af636
Rohan Bagchi <bagchirohan007@gmail.com>
2015-06-05 18:41:41 +0530

    Added route hooks to model

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var App = Ember.Application.create({});
 
 App.Router.map(function(){
     this.resource("Users", { path : '/users' }, function(){
-        this.resourece("User", { path : '/:user_id' }, function(){
+        this.resource("User", { path : '/:user_id' }, function(){
             this.route("EditUser", { path : '/edit' });
         });
         
@@ -10,10 +10,22 @@ App.Router.map(function(){
     });
     
     this.resource("Expenses", { path : '/expenses' }, function(){
-        this.resourece("Expense", { path : '/:expense_id' }, function(){
+        this.resource("Expense", { path : '/:expense_id' }, function(){
             this.route("EditExpense", { path : '/edit' });
         });
         
         this.route("AddExpense", { path : '/add' });
     });
+});
+
+App.UsersRoute = Ember.Route.extend({
+    model : function() {
+        return this.store.find('user');
+    }
+});
+
+App.ExpensesRoute = Ember.Route.extend({
+    model : function() {
+        return this.store.find('expense');
+    }
 });
