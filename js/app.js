37091187918d11e812b8ee40fec2274460017ffe
Rohan Bagchi <bagchirohan007@gmail.com>
2015-06-08 23:09:10 +0530

    general refactoring

--- js/app.js
+++ js/app.js
@@ -5,4 +5,11 @@ App.ApplicationAdapter = DS.FirebaseAdapter.extend({
 });
 
 
-
+App.ApplicationView = Ember.View.extend({
+    currentPathDidChange: function() {
+        Ember.run.next( this, function() {
+            this.$("ul.nav li:has(>a.active)").addClass('active');
+            this.$("ul.nav li:not(:has(>a.active))").removeClass('active');
+        });
+    }.observes('controller.currentPath')
+});
