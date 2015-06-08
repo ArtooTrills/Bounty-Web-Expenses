50cf035dbec85e5d690ea8eca789e0a592321de7
Rohan Bagchi <bagchirohan007@gmail.com>
2015-06-08 22:44:46 +0530

    Refactoring

--- js/controllers/UsersController.js
+++ js/controllers/UsersController.js
@@ -1,4 +1,4 @@
-App.UsersAddController = Ember.ArrayController.extend({
+App.UsersController = Ember.ArrayController.extend({
     actions : {
         addUser : function(){
             var name = this.get('name');
@@ -7,12 +7,10 @@ App.UsersAddController = Ember.ArrayController.extend({
             if (!name.trim()&&!description.trim()){return;}
 
             var user = this.store.createRecord('user', {
-                //'id'            : 'id-' + Math.random().toString(36).substr(2, 16),
                 'name'			: name,
                 'description'	: description
             });
 
-            //this.set('id', '');
             this.set('name', '');
             this.set('description', '');
 
--- js/router.js
+++ js/router.js
@@ -1,11 +1,5 @@
 App.Router.map(function(){
-    this.resource("users", { path : '/users' }, function(){
-        this.resource("user", { path : '/:user_id' }, function(){
-            this.route("edit", { path : '/edit' });
-        });
-        
-        this.route("add", { path : '/add' });
-    });
+    this.resource("users", { path : '/users' });
     
     this.resource("expenses", { path : '/expenses' });
 });
@@ -14,12 +8,6 @@ App.UsersRoute = Ember.Route.extend({
     model : function() {
         return this.store.find('user');
     }
-});
-
-App.UsersAddRoute = Ember.Route.extend({
-	model : function() {
-		return this.modelFor('users');
-	}
 });
 
 App.ExpensesRoute = Ember.Route.extend({
