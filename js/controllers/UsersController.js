13e88698acbe096ef26b1b6b2b4f96440ec244e3
Rohan Bagchi <bagchirohan007@gmail.com>
2015-06-09 00:30:52 +0530

    Update UsersController.js

--- js/controllers/UsersController.js
+++ js/controllers/UsersController.js
@@ -3,17 +3,17 @@ App.UsersController = Ember.ArrayController.extend({
         addUser : function(){
             var name = this.get('name');
             var description = this.get('description');
-
+            // validate not-empty
             if (!name.trim()&&!description.trim()){return;}
 
             var user = this.store.createRecord('user', {
                 'name'			: name,
                 'description'	: description
             });
-
+            // reset fields to empty
             this.set('name', '');
             this.set('description', '');
-
+            // persist
             user.save();
         }
     }
