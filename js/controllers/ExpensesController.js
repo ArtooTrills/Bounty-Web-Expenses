b50d0e5d086c179837a672c82b2e01d6d4be59b8
Rohan Bagchi <bagchirohan007@gmail.com>
2015-06-17 22:05:26 +0530

    Export to CSV implemented for expenses and settlements

--- js/controllers/ExpensesController.js
+++ js/controllers/ExpensesController.js
@@ -12,26 +12,26 @@ App.ExpensesController = Ember.ArrayController.extend({
                 
                 self.transitionTo('expenses');
             });
-        }
-    },
-    export : function(expensesRecord){
-        var data = [['Sl. No.','Spending User', 'Amount', 'Affected Users', 'Time']];
-        for (var i=0;i<expensesRecord.content.length;i++){
-            var expenseObject = expensesRecord.content[i]._data;
-            var spendingUser = expenseObject.spendingUser.name;
-            var amt = expenseObject.amount;
-            var temp = [];
-            for (var j=0;j<expenseObject.affectedUsers.length;j++) {
-                temp.push(expenseObject.affectedUsers[j].name)
-            }
-            temp = temp.toString();
-            var time = expenseObject.createdAt;
-            
-            data.push([i+1, spendingUser, amt, temp, time]);
-            
-            
+        },
+        exportExpenses : function(expensesRecord){
+            var data = [['Sl. No.','Spending User', 'Amount', 'Affected Users', 'Time', 'Date']];
             var csvRows = [];
+            
+            for (var i=0;i<expensesRecord.content.length;i++){
+                var expenseObject = expensesRecord.content[i]._data;
+                var spendingUser = expenseObject.spendingUser.name;
+                var amt = expenseObject.amount;
+                var temp = [];
+                for (var j=0;j<expenseObject.affectedUsers.length;j++) {
+                    temp.push(expenseObject.affectedUsers[j].name)
+                }
+                temp = temp.join("|");
+                var time = moment(expenseObject.createdAt).format('hh:mm:ss a');
+                var date = moment(expenseObject.createdAt).format('DD/MMM/YYYY');
 
+                data.push([i+1, spendingUser, amt, temp, time, date]);
+            }
+            
             for(var i=0, l=data.length; i<l; ++i){
                 csvRows.push(data[i].join(','));
             }
@@ -40,8 +40,34 @@ App.ExpensesController = Ember.ArrayController.extend({
             var a         = document.createElement('a');
             a.href        = 'data:attachment/csv,' + csvString;
             a.target      = '_blank';
-            a.download    = 'myFile.csv';
+            a.download    = 'expenses-'+new Date()+'.csv';
+
+            document.body.appendChild(a);
+            a.click();
+        },
+        exportSettlements : function(payables){
+            var data = [['Sl. No.','Amount', 'By', 'To']];
+            var csvRows = [];
             
+            for (var i=0;i<payables.length;i++){
+                var payableObject = payables[i];
+                
+                var amt = payableObject.amt;
+                var by = payableObject.by;
+                var to = payableObject.to;
+
+                data.push([i+1, amt, by, to]);
+            }
+            for(var i=0, l=data.length; i<l; ++i){
+                csvRows.push(data[i].join(','));
+            }
+
+            var csvString = csvRows.join("%0A");
+            var a         = document.createElement('a');
+            a.href        = 'data:attachment/csv,' + csvString;
+            a.target      = '_blank';
+            a.download    = 'settlements-'+new Date()+'.csv';
+
             document.body.appendChild(a);
             a.click();
         }
