ac2336a53cd60dc5b16b0643ebb74d560b77c0b5
hrishabhshukla <hrishabhkumar@gmail.com>
2016-02-06 17:57:41 +0530

    Read me file update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+                          'importBootstrapTheme': true
+                        }
     // Add options here
   });
 
