d58c1f73a90e9e6d9866dbc5004ccd7c0d825d80
hrishabhshukla <hrishabhkumar@gmail.com>
2016-02-07 02:59:34 +0530

    added export as CSV and Excel buttons

--- app/routes/expence.js
+++ app/routes/expence.js
@@ -4,8 +4,8 @@ export default Ember.Route.extend({
   model: function() {
    return this.store.findAll('expence');
  },
- setupController: function( model) {
-        this.controller.set('model', this.store.findAll('expence'));
+ setupController: function( controller, model) {
+        this.controller.set('model', model);
         this.controller.set('person', this.store.findAll('person'));
     }
 });
