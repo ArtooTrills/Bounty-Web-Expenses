daabb5763ac7906f380bb62e3b9018143148b720
hrishabhshukla <hrishabhkumar@gmail.com>
2016-02-08 04:27:16 +0530

    added loader

---
+++ app/components/spin-button.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    isLoading :false,
+    actions   :{
+                showLoading : function()
+                              {
+                              if(!this.get('isLoading')){
+                                  this.set('isLoading', true);
+                                  this.controller.set('isLoading', true);
+                                  this.sendAction('action', this.get('param'));
+                              }
+        }
+    }
+});
---
+++ tests/integration/components/spin-button-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('spin-button', 'Integration | Component | spin button', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{spin-button}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#spin-button}}
+      template block text
+    {{/spin-button}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
